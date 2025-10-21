22cc042eb2390bd0ec08931bb7ad53f24fde02e8
Chamoswor <kimmern1@gmail.com>
2025-10-21 21:52:22 +0200

    Version

--- cpp/src/binder.cpp
+++ cpp/src/binder.cpp
@@ -272,7 +272,7 @@ PYBIND11_MODULE(_core, m) {
     }, "Create a new VirtualShell instance", py::arg("config"));
 
     // Metadata
-    m.attr("__version__") = "1.1.0";
+    m.attr("__version__") = "1.1.1";
     m.attr("__author__")  = "Kim-Andre Myrvold";
 }
 
--- src/virtualshell/__init__.py
+++ src/virtualshell/__init__.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 try:
     from ._version import version as __version__
 except Exception:
-    __version__ = "1.1.0"
+    __version__ = "1.1.1"
 
 
 from .errors import (
