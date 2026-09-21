e78ca522948c3add7fdd2f1cd1f931b3278d8167
Chamoswor <kimmern1@gmail.com>
2026-09-22 01:26:47 +0200

    Update binder.cpp

--- cpp/src/binder.cpp
+++ cpp/src/binder.cpp
@@ -283,7 +283,7 @@ PYBIND11_MODULE(_core, m) {
     }, "Create a new VirtualShell instance", py::arg("config"));
 
     // Metadata
-    m.attr("__version__") = "1.2.3";
+    m.attr("__version__") = "1.3.0";
     m.attr("__author__")  = "Kim-Andre Myrvold";
 }
 
